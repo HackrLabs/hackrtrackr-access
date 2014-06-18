1e4b32a65a04cc141f9698209a85882c2ffd4ac0
Danny Grove <danny@drgrovellc.com>
2014-06-17 22:06:42 -0400

    Adds config template and reformats server

---
+++ libs/config.template.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var config =
+{ database:
+  { client: 'postgres'
+    , connection:
+    { user: 'hackertracker'
+      , password: 'hackertracker'
+      , host: 'localhost'
+      , database: 'hackertracker'
+    }
+  }
+};
+
+module.exports = config;
+
